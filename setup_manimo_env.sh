1e16799dd2630779119ee4ce6d36dcfdf35101fe
nitro <mohanksriram@gmail.com>
2023-01-19 14:11:17 -0500

    updated setup script

--- setup_manimo_env.sh
+++ setup_manimo_env.sh
@@ -5,10 +5,15 @@ cd ..
 git clone git@github.com:facebookresearch/fairo.git
 cd fairo/polymetis
 
-# # This script sets up the environment for manimo. It is meant to be sourced
+# This script sets up the environment for manimo. It is meant to be sourced
 conda env create -f ./polymetis/environment.yml -n manimo
 conda activate manimo
 pip install -e ./polymetis
+pip install gym
+pip install hydra-core==1.2.0
+pip install dm-control
+pip install dm-robotics-moma
+pip install protobuf==3.20.3
 
 # Build libfranka
 ./scripts/build_libfranka.sh
