64efd978aa60ccc4a1812e276ee065bf33e8c044
jaredmejia <jared.mejia06@gmail.com>
2023-02-23 19:32:46 -0500

    Add replay demo scripts

--- manimo/scripts/collect_teleop_demos_with_logger.py
+++ manimo/scripts/collect_teleop_demos_with_logger.py
@@ -25,17 +25,15 @@ def _get_filename(dir, input, task):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--name")
-    parser.add_argument("--task", type=str, default="insertion")
+    parser.add_argument("--name", type=str, default="demo", help="name of the demo")
     parser.add_argument("--time", type=float, default=100)
-    parser.add_argument("--path", type=str, default="data")
-    parser.add_argument("--gripper_en", type=bool, default= False)
+    parser.add_argument("--path", type=str, default="./demos/")
+    parser.add_argument("--gripper_en", type=bool, default=False)
 
     args = parser.parse_args()
     name = args.name
-    task = args.task
     TIME = args.time
-    HZ = 5
+    HZ = 20
 
     hydra.initialize(
             config_path="../conf", job_name="collect_demos_test"
@@ -43,49 +41,74 @@ def main():
 
     actuators_cfg = hydra.compose(config_name="actuators")
     sensors_cfg = hydra.compose(config_name="sensors")
+    print(f'sensors_cfg: {sensors_cfg}')
 
-    env = SingleArmEnv(sensors_cfg, actuators_cfg)
+    env = SingleArmEnv(sensors_cfg, actuators_cfg, hz=HZ)
     hydra.core.global_hydra.GlobalHydra.instance().clear()
 
     agent = TeleopAgent()
 
-    obs = env.reset()
+    obs, info = env.reset()
 
+    fname = f"{name}.h5"
     while True:
-        data_folder = _create_data_folder(args.path, name, task, index)
-        filename, index = _get_filename(args.path, name, task)
-              
-        logger = DataLogger(filename, save_images=True)
+        save_path = os.path.join(args.path, fname)
+        logger = DataLogger(save_path, save_images=True)
 
         user_in = "r"
         while user_in == "r":
-            obs = env.reset()
-            user_in = input("Ready. Recording {}".format(filename))
+            obs, info = env.reset()
+            user_in = input("Ready. Recording {}".format(save_path))
 
-        step = 0
+        buttons = {}
+        start_logging = False
+        stop_logging = False
+        apply_pos_mask = False
+        logging = False
+        num_obs = 0
         for _ in range(int(TIME * HZ) - 1):
-            start = time.time()
-
-            action = agent.get_action(obs)
-            if action is not None:
-                arm_action, gripper_action, buttons = action
+            arm_action, gripper_action, buttons = agent.get_action(
+                obs, apply_pos_mask=apply_pos_mask
+            )
+            if arm_action is not None:
                 if args.gripper_en:
                     action = [arm_action, gripper_action]
                 else:
                     action = [arm_action]
+            else:
+                action = None
             obs = env.step(action)[0]
-            logger.log(obs)
 
-            step += 1
+            if logging and action is not None:
+                print(f"action: {action}")
+
+                logger.log(obs)
+                num_obs += 1
+
+            if buttons:
+                start_logging = buttons['A']
+                stop_logging = buttons['B']
+                apply_pos_mask = buttons['RTr']
             
-            end = time.time() - start
-        
-        # store the last observation
-        env.reset()
-        logger.finish()
-        index += 1
+            if start_logging and not logging:
+                logging = True
+                print('starting logging')
 
-        print(f"created new directory!")
+            if stop_logging and logging:
+                logging = False
+                print('stopping logging')
+                break
+            
+        print(f'trigger logger finish')
+        logger.finish()
+        print('logger finished')
+
+        print(f'number of observations to be logged: {num_obs}')
+        print("created new directory!")
+
+        env.reset()
+        env.close()
+
         break
 
 if __name__ == "__main__":
