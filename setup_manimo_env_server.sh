a05deebd6c23f7507647f0e65fafbb75e40f8894
nitro <mohanksriram@gmail.com>
2023-07-18 14:00:31 -0400

    update server setup script

--- setup_manimo_env_server.sh
+++ setup_manimo_env_server.sh
@@ -1,21 +1,17 @@
 #! /bin/sh
 
 # Install polymetis from source
-cd ..
-git clone git@github.com:facebookresearch/fairo.git
-cd fairo/polymetis
-git reset --hard 0a01a7fa7a7c65b2f9a3aebf5e79040940daf9d2
+git clone git@github.com:hengyuan-hu/monometis.git
+cd monometis/
+mamba env create -f polymetis/environment.yml -n manimo-latest
+conda activate manimo-latest
 
-# This script sets up the environment for manimo. It is meant to be sourced
-conda env create -f ./polymetis/environment.yml -n manimo
-conda activate manimo
-pip install -e ./polymetis
-
-# Build libfranka
+# compile stuff, no need to build libfranka on this machine
 ./scripts/build_libfranka.sh
-
 mkdir -p ./polymetis/build
 cd ./polymetis/build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_FRANKA=ON
+make -j
+cd ../..
 
-cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_FRANKA=OFF -DBUILD_TESTS=OFF -DBUILD_DOCS=OFF
-make -j 2
+pip install -e ./polymetis
